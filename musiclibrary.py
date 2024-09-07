a8320aff10d7042b023131c42a4b271b97615ad1
abdul rehman <157266271+moiz14553@users.noreply.github.com>
2024-09-07 13:07:23 +0500

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,7 @@
+music={
+    "mashup":"https://youtu.be/5fYpYPzEWDY?si=RRTm00ZLLRmM3Vuk",
+    "mashup2":"https://youtu.be/rDe6QJ4AGQE?si=Zfqn4GCEPXnpPoSD",
+    "yadey teri":"https://youtu.be/15cxCut_WZQ?si=tqjGXheO6rNdGzHA",
+    "coke studio":"https://youtu.be/0SkXKAY5rRQ?si=pgoCKH2xHl3ND1tS"
+
+}
